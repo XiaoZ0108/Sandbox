e307c68c70ea5ce20f9baae25dec7f7795a1294c
Xz0108 <Xiaoxiaoze23@gmail.com>
2025-11-20 14:54:49 +0800

    add docker file

--- rest-controller/src/main/java/com/xz/rest/test/controller/TestController.java
+++ rest-controller/src/main/java/com/xz/rest/test/controller/TestController.java
@@ -3,9 +3,11 @@ package com.xz.rest.test.controller;
 import com.xz.rest.test.service.StringService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/gg")
 public class TestController {
 
     @Autowired
